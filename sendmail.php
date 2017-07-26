1f2cc6a1fa73c1fa883b142b9fc20eef45b14a83
ZPC-PC\Zou <1375489808@qq.com>
2017-07-26 17:29:59 +0800

    版本1.0 基本完成需求=上传图片-发邮件

--- sendmail.php
+++ sendmail.php
@@ -1,48 +1,22 @@
-﻿<?php
-
-	/**
-	 * 已兼容php7
-	 * 注：本邮件类都是经过我测试成功了的，如果大家发送邮件的时候遇到了失败的问题，请从以下几点排查：
-	 * 1. 用户名和密码是否正确；
-	 * 2. 检查邮箱设置是否启用了smtp服务；
-	 * 3. 是否是php环境的问题导致；
-	 * 4. 将26行的$smtp->debug = false改为true，可以显示错误信息，然后可以复制报错信息到网上搜一下错误的原因；
-	 * 5. 如果还是不能解决，可以访问：http://www.daixiaorui.com/read/16.html#viewpl 
-	 *    下面的评论中，可能有你要找的答案。
-	 *
-	 *
-	 * Last update time:2017/06
-	 * UPDATE:
-	 * 1、替换了高版本不支持的写法，如ereg、ereg_replace.
-	 * 2、将 var 改为 public/private等.
-	 * 3、使其兼容php7.
-	 * 
-	 */
-
-	require_once "Smtp.class.php";
-	//******************** 配置信息 ********************************
-	$smtpserver = "smtp.qq.com";//SMTP服务器
-	$smtpserverport =25;//SMTP服务器端口
-	$smtpusermail = "1375489808@qq.com";//SMTP服务器的用户邮箱
-	$smtpemailto = $_POST['toemail'];//发送给谁
-	$smtpuser = "1375489808@qq.com";//SMTP服务器的用户帐号，注：部分邮箱只需@前面的用户名
-	$smtppass = "posdoifpfoqqiicd";//SMTP服务器的用户密码 QQ授权码
-	$mailtitle = $_POST['title'];//邮件主题
-	$mailcontent = "<h1>".$_POST['content']."</h1>";//邮件内容
-	$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
-	//************************ 配置信息 ****************************
-	$smtp = new Smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
-	$smtp->debug = false;//是否显示发送的调试信息
-	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
-
-	echo "<div style='width:300px; margin:36px auto;'>";
-	if($state==""){
-		echo "对不起，邮件发送失败！请检查邮箱填写是否有误。";
-		echo "<a href='index.html'>点此返回</a>";
-		exit();
+<?php
+	require_once("functions.php");
+	$name = $_POST["name"];
+	$phone = $_POST["phone"];
+	$address = $_POST["address"];
+	$bank_account = $_POST["bank_account"];
+	$bank_name = $_POST["bank_name"];
+	$img_sf_1 = $_POST["img_sf_1"];
+	$img_sf_2 = $_POST["img_sf_2"];
+	$img_yh_1 = $_POST["img_yh_1"];
+	$img_yh_2 = $_POST["img_yh_2"];
+	$array_img = array($img_sf_1, $img_sf_2, $img_yh_1, $img_yh_2);
+	$array_content = array($name, "用户姓名：".$name."<br/>用户电话：".$phone."<br/>用户住址：".$address."<br/>银行账户：".$bank_account."<br/>银行名称：".$bank_name);
+	$flag = sendMail($array_img,$array_content);
+	$flag = TRUE;
+	if($flag){
+	    $data['result'] = "发送邮件成功！";
+	}else{
+	    $data['result'] = "发送邮件失败！";
 	}
-	echo "恭喜！邮件发送成功！！";
-	echo "<a href='index.html'>点此返回</a>";
-	echo "</div>";
-
+	echo json_encode($data);
 ?>
